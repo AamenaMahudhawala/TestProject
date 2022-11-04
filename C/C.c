798421d94678a28f5c0b682739906e0b4fd8cfd4
Aamena Mahudhawala <amumahudhawala@gmail.com>
2022-11-04 13:06:00 +0530

    Commit

--- C/C.c
+++ C/C.c
@@ -1,5 +1,49 @@
 #include<stdio.h>
-main()
+
+
+void inputarray(int n,int *ar)
 {
-printf("\nHello");
+	printf("\nEnter the array:-");
+	int i;
+	for(i=0;i<n;i++)
+		scanf("%d",&ar[i]);
+
 }
+main()
+
+{
+
+
+
+int s;
+
+printf("\nEnter lenth of array:-");
+scanf("%d",&s);
+countingarray(s);
+
+
+
+}
+
+
+
+void countingarray(int x)
+{
+	int ar[x],n;
+	inputarray(x,ar);
+	int i,c=0;
+
+	printf("\nEnter the number:-");
+	scanf("%d",&n);
+
+	for(i=0;i<x;i++)
+	{
+		if(ar[i]==n)
+			c+=1;
+	}
+	printf("The number %d occured %d times.",n,c);
+}
+
+
+
+
